f43358fddd9c1fff07fcc7d3186067ce7af2b012
krawthekrow <krawthekrow@gmail.com>
2023-02-25 20:50:58 -0800

    add security safety checks for prod settings

--- settings/prod.py
+++ settings/prod.py
@@ -3,3 +3,7 @@ from settings.base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['.www.example.com'] # FIXME: where are you hosting puzzlord staging?
+
+# security checks
+assert(SECRET_KEY != "FIXME_SECRET_KEY_GOES_HERE")
+assert(SITE_PASSWORD != "FIXME_PASSWORD_GOES_HERE")
